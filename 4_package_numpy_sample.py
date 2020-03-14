07969882265464441181a903974472c50b0c4992
mhmtfrt <32812253+mhmtfrt@users.noreply.github.com>
2020-03-14 18:24:58 +0300

    Update 4_package_numpy_sample.py

--- 4_package_numpy_sample.py
+++ 4_package_numpy_sample.py
@@ -18,4 +18,22 @@ print(amd)
 print(nvidia)
 
 #--------------------------------------------------------#
+import numpy as np
 
+height = [1.73, 1.89, 1.70, 1.82]
+weight = [85, 92 , 80, 80]
+np_height = np.array(height)
+np_weight = np.array(weight)
+
+bmi = np_weight / np_height**2
+
+r_bmi = [round(num, 2) for num in bmi]
+
+print(bmi)
+print(r_bmi)
+print(bmi>25)
+print(bmi[bmi>25])
+#--------------------------------------------------------#
+
+print(height + weight)
+print(np_height + np_weight)
