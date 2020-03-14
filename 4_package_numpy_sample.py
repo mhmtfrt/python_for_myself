e1fb9349a794ce8be2a7f1375c1d2fd0cc997742
mhmtfrt <32812253+mhmtfrt@users.noreply.github.com>
2020-03-14 19:12:57 +0300

    Update 4_package_numpy_sample.py

--- 4_package_numpy_sample.py
+++ 4_package_numpy_sample.py
@@ -2,19 +2,15 @@
 #pip3 install numpy
 
 import numpy
-
 intel = numpy.array(["core","pentium","atom","xeon"])
+print(intel)
 
 import numpy as np
-
 amd = np.array(["ryzen","threadripper","fx"])
+print(amd)
 
 from numpy import array
-
 nvidia = array(["geforce","quadro","jetson"])
-
-print(intel)
-print(amd)
 print(nvidia)
 
 #--------------------------------------------------------#
@@ -33,7 +29,25 @@ print(bmi)
 print(r_bmi)
 print(bmi>25)
 print(bmi[bmi>25])
+
 #--------------------------------------------------------#
 
 print(height + weight)
 print(np_height + np_weight)
+
+#--------------------------------------------------------#
+
+print(type(height))
+print(type(weight))
+print(type(np_height))
+print(type(np_weight))
+
+#--------------------------------------------------------#
+
+np_2d = np.array([height, weight])
+print(np_2d)
+print(np_2d.shape)
+print(np_2d[0])
+print(np_2d[:,2])
+print(np_2d[0][2])
+print(np_2d[0,2])
