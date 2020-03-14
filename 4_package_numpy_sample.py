2df266c258dfeec1d2b2bb85cf8c36725d432f0d
mhmtfrt <32812253+mhmtfrt@users.noreply.github.com>
2020-03-14 18:07:40 +0300

    Create 4_package_numpy_sample.py

---
+++ 4_package_numpy_sample.py
@@ -0,0 +1,21 @@
+#python3 get-pip.py
+#pip3 install numpy
+
+import numpy
+
+intel = numpy.array(["core","pentium","atom","xeon"])
+
+import numpy as np
+
+amd = np.array(["ryzen","threadripper","fx"])
+
+from numpy import array
+
+nvidia = array(["geforce","quadro","jetson"])
+
+print(intel)
+print(amd)
+print(nvidia)
+
+#--------------------------------------------------------#
+
